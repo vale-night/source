c3d1de48c11eb277b3afd6e0468cda2331319fdc
Arthur Tavares <arthurabreu00@gmail.com>
2020-10-26 23:07:30 -0300

    adicionando testes do modulo de pagamento

---
+++ src/br/newtonpaiva/test/PagamentoBoletoTest.java
@@ -0,0 +1,26 @@
+package br.newtonpaiva.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PagamentoBoletoTest {
+
+    @org.junit.jupiter.api.Test
+    void getNumeroBoleto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNumeroBoleto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCpfPaganete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCpfPaganete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void gerarHash() {
+    }
+}
---
+++ src/br/newtonpaiva/test/ServicoPagamentoTest.java
@@ -0,0 +1,18 @@
+package br.newtonpaiva.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicoPagamentoTest {
+
+    @org.junit.jupiter.api.Test
+    void gerarCheckout() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void receberPostback() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mapearStatus() {
+    }
+}
