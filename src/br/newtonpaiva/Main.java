be84eb619d9379ab62d3007f2f086c1d2c116fdb
caioavelar <43555388+caioavelar@users.noreply.github.com>
2020-11-16 18:30:38 -0300

    Criação teste PagamentoCartao

--- src/br/newtonpaiva/Main.java
+++ src/br/newtonpaiva/Main.java
@@ -6,9 +6,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Teste Arquivo: " + testaArquivo());
 		System.out.println("Teste PagamentoBoleto: " + testaPagamentoBoleto());
+		System.out.println("Teste PagamentoCartaoCredito: " + testaPagamentoCartaoCredito());
 		System.out.println("Teste Carrinho: " + testaCarrinho());
-                System.out.println("Teste Cliente" + testacliente());
-                System.out.println("Teste Organizador" + testaorganizador());
+        System.out.println("Teste Cliente: " + testacliente());
+        System.out.println("Teste Organizador: " + testaorganizador());
                 
 	}
 	
@@ -20,7 +21,13 @@ public class Main {
 	public static boolean testaPagamentoBoleto() {
 		PagamentoBoleto pagamento = new PagamentoBoleto();
 		pagamento.setNumeroBoleto("123421");
-		return pagamento.getNumeroBoleto().equals(pagamento.getIdentificador());
+		return pagamento != null && pagamento.getNumeroBoleto().equals(pagamento.getIdentificador());
+	}
+	
+	public static boolean testaPagamentoCartaoCredito() {
+		PagamentoCartaoCredito pagamento = new PagamentoCartaoCredito();
+		pagamento.setHash("hashdecartaodecreditoaleatoria");
+		return pagamento != null && pagamento.getHash().equals(pagamento.getIdentificador());
 	}
 	
 	public static boolean testaCarrinho() {
@@ -28,16 +35,16 @@ public class Main {
 		return carrinho != null && carrinho.getProdutos() != null;
 	}
         
-        public static boolean testacliente(){
-            Cliente cliente = new Cliente();
-            return cliente != null;
-            
-        }
+    public static boolean testacliente(){
+        Cliente cliente = new Cliente();
+        return cliente != null;
         
-        public static boolean testaorganizador(){
-            Organizador organizador = new Organizador();
-            return organizador != null;
-        }
+    }
+    
+    public static boolean testaorganizador(){
+        Organizador organizador = new Organizador();
+        return organizador != null;
+    }
 	
 
 }
